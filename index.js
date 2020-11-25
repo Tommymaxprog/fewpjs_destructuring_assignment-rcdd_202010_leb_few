c2d29ff027e132559807db7303992aa1cc393a8c
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-11-25 09:03:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -45,10 +45,10 @@ const [,,,,,indg,]=colors;
 // Objects
 
 // 7. Use destructuring to assign all appropriate variables using the keys as the variable names
-const [name,color,song,job,partner]=muppet;
-const [nodejs,k_color,k_album,,k_job,k_partner]=k_muppet;
+const {name,color,song,job,partner}=muppet;
+const {k_name,k_color,k_album,,k_job,k_partner}=k_muppet;
 
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
-const[,song_2,,song_4]=k_muppet.k_album.the_muppet_movie;
-const[,kcolor,,kjob,kpartner]=k_muppet;
+const{,song_2,,song_4}=k_muppet.k_album.the_muppet_movie;
+const{,kcolor,,kjob,kpartner}=k_muppet;
